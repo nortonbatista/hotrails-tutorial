764cde6279398bd90e2fae91347dc69a37dd8641
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2024-02-15 19:56:42 -0300

    Scaffold quotes crud

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,55 @@
+class QuotesController < ApplicationController
+  before_action :set_quote, only: %i[show edit update destroy]
+
+  def index
+    @quotes = Quote.all
+  end
+
+  def show; end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def edit; end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    respond_to do |format|
+      if @quote.save
+        format.html { redirect_to quote_url(@quote), notice: 'Quote was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @quote.update(quote_params)
+        format.html { redirect_to quote_url(@quote), notice: 'Quote was successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @quote.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to quotes_url, notice: 'Quote was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quote_params
+    params.require(:quote).permit(:name)
+  end
+end
---
+++ spec/views/quotes/edit.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'quotes/edit' do
+  let(:quote) do
+    Quote.create!(
+      name: 'MyString'
+    )
+  end
+
+  before do
+    assign(:quote, quote)
+  end
+
+  it 'renders the edit quote form' do
+    render
+
+    assert_select 'form[action=?][method=?]', quote_path(quote), 'post' do
+      assert_select 'input[name=?]', 'quote[name]'
+    end
+  end
+end
